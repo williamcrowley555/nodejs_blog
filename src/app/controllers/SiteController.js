aa31d764a314f0be68a636916283ef485e6d92e6
William <buiquanghuy.10122000.10@gmail.com>
2023-02-01 23:45:09 +0700

    Read course from db

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,13 +1,22 @@
-class SiteController {
-    // [GET] /
-    index(req, res) {
-        res.render('home');
-    }
+const Course = require("../models/Course");
+const { multipleMongooseToObject } = require("../../util/mongoose");
 
-    // [GET] /search
-    search(req, res) {
-        res.render('search');
-    }
+class SiteController {
+  // [GET] /
+  index(req, res, next) {
+    Course.find({})
+      .then((course) =>
+        res.render("home", {
+          courses: multipleMongooseToObject(course),
+        })
+      )
+      .catch(next);
+  }
+
+  // [GET] /search
+  search(req, res) {
+    res.render("search");
+  }
 }
 
 module.exports = new SiteController();
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
-const newsRoute = require('./news');
-const siteRoute = require('./site');
+const newsRoute = require("./news");
+const coursesRoute = require("./courses");
+const siteRoute = require("./site");
 
 function route(app) {
-    app.use('/news', newsRoute);
-    app.use('/', siteRoute);
+  app.use("/news", newsRoute);
+  app.use("/courses", coursesRoute);
+
+  app.use("/", siteRoute);
 }
 
 module.exports = route;
