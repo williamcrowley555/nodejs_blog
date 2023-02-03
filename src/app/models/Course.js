ab0ad54470c89bac16f6c33c31fac71dfbcc783d
William <buiquanghuy.10122000.10@gmail.com>
2023-02-03 23:23:47 +0700

    feat: Create new course

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,12 +1,22 @@
 const mongoose = require("mongoose");
+const slug = require("mongoose-slug-generator");
+
+mongoose.plugin(slug);
+
 const Schema = mongoose.Schema;
 
-const Course = new Schema({
-  name: { type: String, maxlength: 255 },
-  descryption: { type: String, maxlength: 600 },
-  image: { type: String, maxlength: 255 },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-});
+const Course = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String },
+    image: { type: String },
+    videoId: { type: String, required: true },
+    level: { type: String },
+    slug: { type: String, slug: "name", unique: true },
+  },
+  {
+    timestamp: true,
+  }
+);
 
 module.exports = mongoose.model("Course", Course);
